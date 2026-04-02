a9d28f96b3414d6e418023ee02990b6c69403fbe
28ana <markovicana316@gmail.com>
2026-04-02 22:34:28 +0200

    izmena3

--- config/dbcon.php
+++ config/dbcon.php
@@ -23,22 +23,44 @@
     //     die("PDO GREŠKA: " . $ex->getMessage());
     // }
 
-$host = getenv('MYSQLHOST');
+// $host = getenv('MYSQLHOST');
+// $db   = getenv('MYSQLDATABASE');
+// $user = getenv('MYSQLUSER');
+// $pass = getenv('MYSQLPASSWORD');
+// $port = getenv('MYSQLPORT');
+
+// try {
+//     $conn = new PDO(
+//         "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+//         $user,
+//         $pass
+//     );
+
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// } catch (PDOException $e) {
+//     die("Greška pri povezivanju: " . $e->getMessage());
+// } 
+
+// Railway automatski popunjava ove promenljive ako su definisane u "Variables" tabu
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$port = getenv('MYSQLPORT') ?: '3306';
 $db   = getenv('MYSQLDATABASE');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $conn = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass
-    );
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-    die("Greška pri povezivanju: " . $e->getMessage());
-} 
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     // echo "Uspešna konekcija!"; 
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
 ?>
